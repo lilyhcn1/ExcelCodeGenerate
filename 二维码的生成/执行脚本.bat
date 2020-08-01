fa84314c27cf35906345a757dbf10750b3a72c14
lilyhcn1 <lilyhcn1@upsir.com>
2020-08-01 21:22:54 +0800

    新增二维码生成，修改通讯录发消息

---
+++ 二维码的生成/执行脚本.bat
@@ -0,0 +1,76 @@
+ 
+ 
+rem 吾爱破解
+set nowpath=%~dp0
+set notepad2path=%nowpath%辅助文件夹\notepad2\notepad2.exe
+set zint=%nowpath%辅助文件夹\zint二维码生成\zint.exe
+set genpath=%nowpath%生成文件夹
+cd %genpath%
+echo 吾爱破解 >temp.txt
+%zint% -o %genpath%\吾爱破解.png  -b 58 --scale 10 --binary -i temp.txt 
+rem 原创作者
+set nowpath=%~dp0
+set notepad2path=%nowpath%辅助文件夹\notepad2\notepad2.exe
+set zint=%nowpath%辅助文件夹\zint二维码生成\zint.exe
+set genpath=%nowpath%生成文件夹
+cd %genpath%
+echo 原创作者 >temp.txt
+%zint% -o %genpath%\原创作者.png  -b 58 --scale 10 --binary -i temp.txt 
+rem 22
+2
+set nowpath=%~dp0
+set notepad2path=%nowpath%辅助文件夹\notepad2\notepad2.exe
+set zint=%nowpath%辅助文件夹\zint二维码生成\zint.exe
+set genpath=%nowpath%生成文件夹
+cd %genpath%
+echo 22
+2 >temp.txt
+%zint% -o %genpath%\444444.png  -b 58 --scale 10 --binary -i temp.txt 
+rem lilyhcn
+set nowpath=%~dp0
+set notepad2path=%nowpath%辅助文件夹\notepad2\notepad2.exe
+set zint=%nowpath%辅助文件夹\zint二维码生成\zint.exe
+set genpath=%nowpath%生成文件夹
+cd %genpath%
+echo lilyhcn >temp.txt
+%zint% -o %genpath%\lilyhcn.png  -b 58 --scale 10 --binary -i temp.txt 
+rem https://www.52pojie.cn/
+set nowpath=%~dp0
+set notepad2path=%nowpath%辅助文件夹\notepad2\notepad2.exe
+set zint=%nowpath%辅助文件夹\zint二维码生成\zint.exe
+set genpath=%nowpath%生成文件夹
+cd %genpath%
+echo https://www.52pojie.cn/ >temp.txt
+%zint% -o %genpath%\52pojie.png  -b 58 --scale 10 --binary -i temp.txt 
+rem http://www.baidu.com
+set nowpath=%~dp0
+set notepad2path=%nowpath%辅助文件夹\notepad2\notepad2.exe
+set zint=%nowpath%辅助文件夹\zint二维码生成\zint.exe
+set genpath=%nowpath%生成文件夹
+cd %genpath%
+echo http://www.baidu.com >temp.txt
+%zint% -o %genpath%\ssfdf.png  -b 58 --scale 10 --binary -i temp.txt 
+rem 空白
+set nowpath=%~dp0
+set notepad2path=%nowpath%辅助文件夹\notepad2\notepad2.exe
+set zint=%nowpath%辅助文件夹\zint二维码生成\zint.exe
+set genpath=%nowpath%生成文件夹
+cd %genpath%
+echo 空白 >temp.txt
+%zint% -o %genpath%\空白.png  -b 58 --scale 10 --binary -i temp.txt 
+rem 我是右  城村奇才厅无可奈何地厅无可奈何地大本营魂牵梦萦地大本营需要d  革大本营魂牵梦萦富士达地李斐莉雪枯地阿斯蒂芬
+set nowpath=%~dp0
+set notepad2path=%nowpath%辅助文件夹\notepad2\notepad2.exe
+set zint=%nowpath%辅助文件夹\zint二维码生成\zint.exe
+set genpath=%nowpath%生成文件夹
+cd %genpath%
+echo 我是右  城村奇才厅无可奈何地厅无可奈何地大本营魂牵梦萦地大本营需要d  革大本营魂牵梦萦富士达地李斐莉雪枯地阿斯蒂芬 >temp.txt
+%zint% -o %genpath%\333333333333.png  -b 58 --scale 10 --binary -i temp.txt 
+rem 222
+set nowpath=%~dp0
+set notepad2path=%nowpath%辅助文件夹\notepad2\notepad2.exe
+set zint=%nowpath%辅助文件夹\zint二维码生成\zint.exe
+set genpath=%nowpath%生成文件夹
+cd %genpath%
+echo 222 >temp.txt
+%zint% -o %genpath%\333.png  -b 58 --scale 10 --binary -i temp.txt 
