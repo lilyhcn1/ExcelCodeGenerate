5fee867bcf58e91fb4affc44ab6fdaf59926c9b8
lilyhcn1 <lilyhcn1@upsir.com>
2021-08-25 10:33:49 +0800

    111111

---
+++ 老黄牛小工具/脚本文件/r34fun.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from urllib import request
+import urllib.parse
+import os,os.path,json,sys
+import requests
+import time
+MOBAN="模板"
+GENPATH="生成文件夹"
+JSONFILE=r'd:/老黄牛小工具/ExcelQuery/temp/temp.json'
+JSONFILE2=r'd:/老黄牛小工具/ExcelQuery/temp/temp2.json'
+TEMPTEXT=r'd:/老黄牛小工具/ExcelQuery/temp/temp.txt'
+TEMPJPG=r'D:/老黄牛小工具/ExcelQuery/temp/temp.jpg'
+SCREENEREXE=r'd:/老黄牛小工具/小工具/360screener.exe'
+NCONVERTEXE=r'D:/老黄牛小工具/小工具/缩小图片nconvert/nconvert.exe'
+
+
+#这里替换成常用函数
+def savearr2json(arr,wtype="key"):
+    with open(JSONFILE, 'w') as file_object:
+        sc=os.path.splitext(os.path.basename(__file__))[0]
+        arr2={}
+        arr2['script']=sc
+        arr2['w']=wtype
+        arr2['content']=arr
+        file_object.write(json.dumps(arr2))
+        
+def savestr(str):
+    with open(JSONFILE, 'w') as file_object:
+        file_object.write(str)
+
+def readjson():
+    with open(JSONFILE,'r',encoding='utf8') as f:
+        ct = f.read()
+        if ct.startswith(u'\ufeff'):
+            ct = ct.encode('utf8')[3:].decode('utf8')
+            ct = ct.replace('\\','\\\\')
+    jsonarr = json.loads(ct)
+    listarr = jsonarr['contents']
+    return listarr
+def firstval(arr):
+    for index in arr:
+        return arr[index]
+
+
+def savesttxt(str,path=TEMPTEXT):
+    with open(path, 'w') as file_object:
+        file_object.write(str)
+        
+def r34sysargv(inputstr=""):
+    if len(sys.argv)>1:    #判断有没有参数
+        n = len(sys.argv)
+        temp=""
+        for i in range(1, n):
+            if i == 1 :
+                temp = temp + sys.argv[i]
+            else:
+                temp = temp + sys.argv[i] +" "
+        inputstr=temp
+        #print("input: "+ inputstr)
+    return inputstr
+
+
+def screen2jpg():
+    os.system(SCREENEREXE)
+    exeurl = NCONVERTEXE + " -out jpeg -clipboard -overwrite -o " + TEMPJPG
+    print(exeurl)
+
+#-----------------------------------------------------
+
+
+
